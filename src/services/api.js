06841666c52f07d4595ed1e54d625496aeb5acbe
sumit chauhan <140949670+SumitChauhan321@users.noreply.github.com>
2026-05-19 15:05:26 +0530

    Update BASE_URL to production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "team-task-manager-production-d9e2.up.railway.app";
 
 export const apiFetch = async (url, method = "GET", body = null) => {
   const token = localStorage.getItem("token");
