f79af54742fd2884208d171b626d54347ea3c3eb
sumit chauhan <140949670+SumitChauhan321@users.noreply.github.com>
2026-05-19 15:11:04 +0530

    Update BASE_URL to include HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "team-task-manager-production-d9e2.up.railway.app";
+export const BASE_URL = "https://team-task-manager-production-d9e2.up.railway.app";
 
 export const apiFetch = async (url, method = "GET", body = null) => {
   const token = localStorage.getItem("token");
